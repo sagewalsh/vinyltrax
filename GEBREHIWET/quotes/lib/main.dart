3d2e05a7222a6c4f5930154cdc6b256b67729b2b
Yoel <73871471+yoelgeb@users.noreply.github.com>
2022-06-23 17:43:56 -0700

    Add files via upload

---
+++ GEBREHIWET/quotes/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Osca Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Osca Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Osca Wilde', text: 'The truth is rarely pure and never simple')
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+            }
+        )).toList(),
+      ),
+    );
+  }
+}
+
