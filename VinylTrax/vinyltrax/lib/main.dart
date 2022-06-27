6715189cee3afb40ee9d454ed13534ce83d39eb4
sagewalsh <sagewalsh@gmail.com>
2022-06-27 11:56:49 -0700

    firebase connected

--- VinylTrax/vinyltrax/lib/main.dart
+++ VinylTrax/vinyltrax/lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import './tabs.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const VinylTrax());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(VinylTrax());
+}
 
 class VinylTrax extends StatelessWidget {
-  const VinylTrax({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
+  VinylTrax({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // home:
+    FutureBuilder(
+        future: _fbApp,
+        builder: (contrext, snapshot) {
+          if (snapshot.hasError) {
+            print("You have an error! ${snapshot.error.toString()}");
+            return Text("Something went wrong!");
+            // } else if (snapshot.hasData) {
+            // return MyHomePage(title: "Vinyl Trax");
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
     return MaterialApp(home: Tabs());
   }
 }
--- VinylTrax/vinyltrax/test/widget_test.dart
+++ VinylTrax/vinyltrax/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:vinyltrax/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(VinylTrax());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
