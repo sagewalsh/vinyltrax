7b73450562f236b622b2f5de2ea339dd738683eb
sagewalsh <sagewalsh@gmail.com>
2022-06-27 10:30:10 -0700

    files copied

---
+++ Prototype1/prototype1/lib/searchpage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './textinput.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 212, 212, 212),
+      appBar: AppBar(
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: const Text("Search Page"),
+        ),
+      ),
+      body: const TextInput(),
+    );
+  }
+}
