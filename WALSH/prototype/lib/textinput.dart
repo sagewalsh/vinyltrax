f3e3296e862838cdae95dabf6bf0dbb50b66b1cd
sagewalsh <sagewalsh@gmail.com>
2022-06-27 09:58:44 -0700

    search bar

--- WALSH/prototype/lib/textinput.dart
+++ WALSH/prototype/lib/textinput.dart
@@ -5,14 +5,15 @@ class TextInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
           padding: EdgeInsets.all(8.0),
           child: TextField(
             decoration: InputDecoration(
               labelText: "Search",
               hintText: "Artist, Album, Song",
-              border: OutlineInputBorder(),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
               suffixIcon: Icon(Icons.search),
             ),
           )),
