b01a892f905fbf3fc6aab773f411df38230c5c43
Yoel <yoelgebre@gmail.com>
2022-06-27 12:29:28 -0700

    Made icon and list buttons

--- VinylTrax/vinyltrax/lib/searchpage.dart
+++ VinylTrax/vinyltrax/lib/searchpage.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-import './textinput.dart';
+import 'package:flutter/cupertino.dart';
+import 'textinput.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+enum _Tab {one, two}
+
+class _SearchPageState extends State<SearchPage> {
+  _Tab _selectedTab = _Tab.one;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 212, 212, 212),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Container(
           width: double.infinity,
@@ -15,7 +25,49 @@ class SearchPage extends StatelessWidget {
           child: const Text("Search Page"),
         ),
       ),
-      body: const TextInput(),
+      body: Column(
+        children: [
+          TextInput(),
+          Row(
+            children: [
+              CupertinoSegmentedControl(
+                padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                selectedColor: Colors.black,
+                borderColor: Colors.black,
+                pressedColor: Colors.grey,
+                children: {
+                  _Tab.one: Container(
+                    height: 30,
+                    width: 70,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    child: Center(
+                      child: Text("Icon"),
+                    ),
+                  ),
+                  _Tab.two: Container(
+                    height: 20,
+                    width: 70,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    child: Center(
+                      child: Text("List"),
+                    ),
+                  ),
+                },
+                onValueChanged: (value) {
+                  setState(() {
+                    _selectedTab = value as _Tab;
+                  });
+                },
+                groupValue: _selectedTab,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
