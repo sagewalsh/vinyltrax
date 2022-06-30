5b1ffb8b8e3acd5b085ca46457c94f64a690008a
Yoel <yoelgebre@gmail.com>
2022-06-30 13:24:37 -0700

    Fixed up genre, made then clickable

--- VinylTrax/vinyltrax/lib/show_data/genre.dart
+++ VinylTrax/vinyltrax/lib/show_data/genre.dart
@@ -1,84 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:vinyltrax/show_data/genreTile.dart';
 
 class GenreList extends StatelessWidget {
   GenreList({Key? key}) : super(key: key);
 
-  List<ListTile> genreList = [
-    ListTile(
-      title: Text("Blues"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Brass and Military"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Children's"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Classical"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Electronic"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Folk, World, and Country"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Funk/Soul"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Hip-Hop"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Jazz"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Latin"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Non-Music"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Pop"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Reggae"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Rock"),
-      tileColor: Colors.black12,
-      visualDensity: VisualDensity(vertical: -3),
-    ),
-    ListTile(
-      title: Text("Stage and Screen"),
-      tileColor: Colors.white,
-      visualDensity: VisualDensity(vertical: -3),
-    )
+  List<GenreTile> genreList = [
+    GenreTile("Blues", false),
+    GenreTile("Brass and Military", true),
+    GenreTile("Children's", false),
+    GenreTile("Classical", true),
+    GenreTile("Electronic", false),
+    GenreTile("Folk, Work, and Country", true),
+    GenreTile("Funk / Soul", false),
+    GenreTile("Hip-Hop", true),
+    GenreTile("Jazz", false),
+    GenreTile("Latin", true),
+    GenreTile("Non-Music", false),
+    GenreTile("Pop", true),
+    GenreTile("Reggae", false),
+    GenreTile("Rock", true),
+    GenreTile("Stage and Screen", false),
   ];
 
   @override
